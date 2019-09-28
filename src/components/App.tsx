22d8f9070d19dfb43b994212ebc2303cb1b39595
Nolan <nolan.a.schwartz@gmail.com>
2019-09-28 14:49:54 -0700

    add storybook + prettify

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      TwitchCon!
-    </div>
-  );
+  return <div className="App">TwitchCon!</div>;
 };
 
 export default App;
