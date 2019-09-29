f00912291da74495156d86db4e7a8de13ff2200c
redxzeta <xzetanzero@gmail.com>
2019-09-29 01:30:10 -0700

    added card ,bolded active, centerted badge icon and name

--- src/components/BadgeInfo/BadgeStatus.jsx
+++ src/components/BadgeInfo/BadgeStatus.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import BadgeIcon from "../Badges/BadgeIcon";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+const useStyles = makeStyles({
+  card: {
+    minWidth: 200,
 
+  },
+
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 15,
+  },
+});
 // activate button should route to Confirm Menu
 
 function BadgeStatus(props) {
@@ -8,8 +24,8 @@ function BadgeStatus(props) {
     !props.badge.active && props.badge.awarded ? (
       <button>activate</button>
     ) : (
-      <span>active</span>
-    );
+        <span>active</span>
+      );
 
   if (!props.badge.awarded) {
     button = null;
@@ -17,10 +33,23 @@ function BadgeStatus(props) {
 
   return (
     <div>
-      <p>{props.badge.active ? "Active Badge" : "Earning Badge"}</p>
-      <BadgeIcon badge={props.badge} />
-      <p>{props.badge.name}</p>
-      {button}
+
+      <Card className={useStyles.card}>
+        <CardContent>
+          <Typography className={useStyles.title} color="textSecondary" gutterBottom>
+            <h4 style={{ color: 'black', fontWeight: "bold" }}> {props.badge.active ? "Active Badge\n" : "Earning Badge"}</h4>
+          </Typography>
+
+          <center>
+            <BadgeIcon badge={props.badge} />
+            <p>{props.badge.name}</p>
+          </center>
+
+          {button}
+        </CardContent>
+
+      </Card>
+
     </div>
   );
 }
