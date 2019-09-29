88ef96a44deeaa5a41e8341480a8a501da98f8a3
Nolan <nolan.a.schwartz@gmail.com>
2019-09-29 10:56:42 -0700

    fix list items

--- src/components/Badges/BadgeListItem.jsx
+++ src/components/Badges/BadgeListItem.jsx
@@ -1,43 +1,19 @@
 import React from "react";
 import BadgeIcon from "./BadgeIcon";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import CardHeader from '@material-ui/core/CardHeader';
-const useStyles = makeStyles({
-  card: {
-    minWidth: 200,
-
-  },
-
-  title: {
-    fontSize: 12,
-
-  },
-  pos: {
- 
-  },
-
-});
-// activate button should route to Confirm Menu
-
-
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
 
 function BadgeListItem(props) {
-
   return (
-    <Card className={useStyles.card}>
-          <CardHeader
-         avatar={
-          <BadgeIcon badge={props.badge} />
-        }
-        title={props.badge.fullName}
-        subheader= {props.badge.eventDate}
-      />
-  </Card>
-   
+    <div style={{ marginBottom: "10px" }}>
+      <Card>
+        <CardHeader
+          avatar={<BadgeIcon badge={props.badge} />}
+          title={props.badge.fullName}
+          subheader={props.badge.eventDate}
+        />
+      </Card>
+    </div>
   );
 }
 
