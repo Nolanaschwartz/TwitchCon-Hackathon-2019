f9b31dddc307626f4b6abde7f841df31ffda9b06
redxzeta <xzetanzero@gmail.com>
2019-09-29 09:59:52 -0700

    stylin badge list

--- src/components/Badges/BadgeListItem.jsx
+++ src/components/Badges/BadgeListItem.jsx
@@ -1,15 +1,46 @@
 import React from "react";
 import BadgeIcon from "./BadgeIcon";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+const useStyles = makeStyles({
+  card: {
+    minWidth: 200,
+
+  },
+
+  title: {
+    fontSize: 12,
+
+  },
+  pos: {
+ 
+  },
+});
+// activate button should route to Confirm Menu
+
+
 
 function BadgeListItem(props) {
   // TODO: Add styling
 
   return (
-    <li>
+
+
+    <Card className={useStyles.card}>
+    <CardContent>
+      <Typography className={useStyles.title} variant="headline" color="textSecondary" gutterBottom>
       <BadgeIcon badge={props.badge} />
-      <p>{props.badge.fullName}</p>
-      <p>Earned: {props.badge.eventDate}</p>
-    </li>
+      <h4 style={{ color: 'black', fontWeight: "bold" }}>  {props.badge.fullName}</h4>   
+      </Typography>
+      <Typography variant="body2" component="p">
+      Earned: {props.badge.eventDate}
+        </Typography>
+    </CardContent>
+
+  </Card>
+   
   );
 }
 
