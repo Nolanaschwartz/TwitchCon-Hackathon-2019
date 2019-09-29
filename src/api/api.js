0805a65caba45eb582fd5d979acebc5252867691
Nathaniel Michael <natecmichael@gmail.com>
2019-09-29 10:50:52 -0700

    started change active badge

--- src/api/api.js
+++ src/api/api.js
@@ -10,3 +10,22 @@ export async function fetchBadgesByUser(userId) {
 
   return data.data;
 }
+
+export async function changeActiveBadge(userId, badgeId) {
+  userId = "LithHarbor";
+
+  const userData = await axios.get(API_BASEURL + "/users/" + userId);
+  const user = userData.data;
+
+  for (var badge in user.badges) {
+    if (badge.active) {
+      badge.active = false;
+    }
+
+    if (badge.id === badgeId) {
+      badge.active = true;
+    }
+  }
+
+  console.log(user);
+}
--- src/modules/badges.js
+++ src/modules/badges.js
@@ -5,12 +5,15 @@ import * as api from "../api/api";
 const FETCHING_BADGES = "badges/fetching";
 const FETCHED_BADGES = "badges/fetched";
 const FETCH_BADGES_ERROR = "badges/fetch/error";
+const CHANGING_ACTIVE_BADGE = "badges/change/active";
+const CHANGING_ACTIVE_BADGE_ERROR = "badges/change/active/error";
 
 // initial state
 
 const INITIAL_STATE = {
   fetching: false,
   badges: [],
+  activeBadge: null,
   fetchError: false
 };
 
@@ -21,7 +24,15 @@ export default function reducer(state = INITIAL_STATE, action) {
     case FETCHING_BADGES:
       return { ...state, fetching: true };
     case FETCHED_BADGES:
-      return { ...state, fetching: false, badges: action.payload };
+      let activeBadge = null;
+      action.payload.map(item => {
+        if (item.active) {
+          activeBadge = item;
+          return item;
+        }
+        return null;
+      });
+      return { ...state, fetching: false, badges: action.payload, activeBadge };
     case FETCH_BADGES_ERROR:
       return { ...state, fetching: false, fetchError: true };
     default:
